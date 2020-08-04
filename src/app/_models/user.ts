430f3c44c55258746765091856fc966afe68e8f3
Sudhir_Kumar <k.sdh99@gmail.com>
2020-08-04 17:15:05 +0530

    first commit

---
+++ src/app/_models/user.ts
@@ -0,0 +1,14 @@
+﻿export class User {
+    id: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+    employeeName: string;
+    companyName: string;
+    experience: number;
+    salry: number;
+    emailId: string;
+    role: string;
+}
